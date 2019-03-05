09e0743756ce6a297ab194edfd607aafb74240b9
Egor <45356764+egoraleksandrov1ch@users.noreply.github.com>
2019-03-05 16:10:37 +0300

    any_message_here

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 // this is an example of improting data from JSON
-import 'orders' from '../data/orders.json';
+// import 'orders' from '../data/orders.JSON';
 
-export default (function () {
-    // YOUR CODE GOES HERE
-    // next line is for example only
-    document.getElementById("app").innerHTML = "<h1>Hello WG Forge</h1>";
-}());
+// export default (function () {
+//     // YOUR CODE GOES HERE
+//     // next line is for example only
+//     document.getElementById("app").innerHTML = "<h1>Hello WG Forge</h1>";
+// }());
+
+let tableApp = {};
+
+tableApp.init
