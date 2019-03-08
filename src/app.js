96f28e87a8f03a5d496efe3643ea299385818a2f
Egor <45356764+egoraleksandrov1ch@users.noreply.github.com>
2019-03-08 20:55:10 +0300

    any_message_here

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@
 // import 'orders' from '../data/orders.JSON';
 
 export default (function () {
-    
-    let urlUsers = 'http://localhost:9000/api/users.json';
-    let urlCompanies = 'http://localhost:9000/api/Companies.json';
 
     function init() {
         getApi('http://localhost:9000/api/orders.json')
@@ -28,7 +25,7 @@ export default (function () {
             req.open('GET', url, true);
             req.responseType = 'json';
             req.send();
-        }) 
+        }); 
     };
 
     function roundArr(obj) {
@@ -110,8 +107,79 @@ export default (function () {
             userLink.textContent = `Ms. ${obj.first_name} ${obj.last_name}`;
         }
         elem.appendChild(userLink);
+
+        let infoUser = document.createElement('div');
+        infoUser.classList.add('user-details');
+        infoUser.style.display = 'none';
+        elem.appendChild(infoUser);
+
+        constructorInfoUser(obj, infoUser);
+
+        let k = true;
+        userLink.onclick = () => {
+            infoUser.style.display = k ? 'block' : 'none';
+            k = !k;
+            // console.log('hi');
+        };
     };
 
+    function constructorInfoUser(objUser, elem) {
+        let birthday = document.createElement('p');
+        if (objUser.birthday !== null) {
+            let time = new Date(1970, 0, 1);
+            time.setSeconds(objUser.birthday);
+            let monthTime = time.getMonth() + 1;
+            if (time.getMonth() < 9) {
+                monthTime = '0' + monthTime;
+            }
+            let editTime = `${time.toString()[8] + time.toString()[9]}/${monthTime}/${time.getFullYear()}`;
+            birthday.textContent = `Birthday: ${editTime}`;
+        } 
+        else {
+            birthday.textContent = 'Brithday: missing.';
+        }
+        elem.appendChild(birthday);
+
+        let photoUser = document.createElement('p');
+        let imgUser = document.createElement('img');
+        imgUser.setAttribute('width', '100px');
+        imgUser.setAttribute('src', objUser.avatar);
+        photoUser.appendChild(imgUser);
+        elem.appendChild(photoUser);
+
+        getApi('http://localhost:9000/api/companies.json')
+		.then( response => {
+            // console.log(objUser);
+            for (let i = 0; i <= response.length; i++) {
+                if (response[i].id == objUser.company_id) {
+                    constructorCompanies(response[i], elem);
+                }
+                else if (objUser.company_id == null) {
+                    let companyUser = document.createElement('p');
+                    companyUser.textContent = 'Company: missing.';
+                    elem.appendChild(companyUser);
+                    break;
+                }
+            };
+        });
+    };
+
+    function constructorCompanies(obj, elem) {
+        let companyUser = document.createElement('p');
+        companyUser.textContent = 'Company: ';
+
+        let nameCompany = document.createElement('a');
+        nameCompany.setAttribute('href', obj.url);
+        nameCompany.textContent = obj.title;
+        companyUser.appendChild(nameCompany);
+        elem.appendChild(companyUser);
+
+        if (obj.industry !== 'n/a') {
+            let industry = document.createElement('p');
+            industry.textContent = `Industry: ${obj.industry}`;
+            elem.appendChild(industry);
+        };
+    };
 
     init();
     // document.getElementById("app").innerHTML = "<h1>Hello WG Forge</h1>";
